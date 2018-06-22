59eefbe253e89e920d7e78743f54467f167a4721
Robiel <robielasgedom@gmail.com>
2018-06-21 23:52:02 -0400

    update timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,5 @@
 $('#start').on("click",function(){
-    for(var i=0; i<questions.length; i++) {
-        $('#inside').append('<h2>' + questions[i].question + '</h2>')
-        for(var j=0; j < questions[i].answers.length, j++;){
-            $('#inside').append("<input type='radio' name='question-" +i+"' value='" + questions[i].answers[j]+"'>" +questions[i].answers[j])
-        }
-    }
+   
 })
 
 var questions = [ {
@@ -28,3 +23,28 @@ var questions = [ {
     answers: ["True", "False", "Orange", "Green"],
     correctAnswer: "Orange"
 }];
+console.log(questions);
+
+var game = {
+    correct: 0,
+    incorrect: 0,
+    counter: 120,
+    countdown: function(){
+        game.counter--;
+        $('#counter').html(game.counter);
+        if(game.counter<=0){
+            console.log("time is up");
+            game.done();
+
+        }
+    },
+    $('#start').remove();
+    timer = setInterval(game.timer,1000);
+
+    for(var i=0; i<questions.length; i++) {
+        $('#subwrapper').append('<h2>' + questions[i].question + '</h2>')
+        for(var j=0; j < questions[i].answers.length, j++;){
+            $('#subwrapper').append("<input type='radio' name='question-" +i+"' value='" + questions[i].answers[j]+"'>" +questions[i].answers[j])
+        }
+    }
+}
