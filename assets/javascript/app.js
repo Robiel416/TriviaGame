c38af024862911ea5cf5d6a157e9f69dd82e2a55
Robiel <robielasgedom@gmail.com>
2018-06-21 23:35:16 -0400

    function start

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,11 @@
 $('#start').on("click",function(){
-  
+    for(var i=0; i<questions.length; i++) {
+        $('#inside').append('<h2>' + questions[i].question + '</h2>')
+        for(var j=0; j < questions[i].answers.length, j++;){
+            $('#inside').append("<input type='radio' name='question-" +i+"' value='" + questions[i].answers[j]+"'>" +questions[i].answers[j])
+        }
+    }
+})
 
 var questions = [ {
     question: "what is this question?",
