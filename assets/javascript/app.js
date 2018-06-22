f0102ce16e408dc2b92bd35a66c4b3a8be8f5299
Robiel <robielasgedom@gmail.com>
2018-06-21 23:18:23 -0400

    intial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+$('#start').on("click",function(){
+  
+
+var questions = [ {
+    question: "what is this question?",
+    answers: ["True", "False", "Orange", "Green"],
+    correctAnswer: "Orange"
+}, {
+    question: "what is this question?",
+    answers: ["True", "False", "Orange", "Green"],
+    correctAnswer: "False"
+}, { 
+    question: "what is this question?",
+    answers: ["True", "False", "Orange", "Green"],
+    correctAnswer: "Green"
+}, {
+    question: "what is this question?",
+    answers: ["True", "False", "Orange", "Green"],
+    correctAnswer: "Orange"
+}, {
+    question: "what is this question?",
+    answers: ["True", "False", "Orange", "Green"],
+    correctAnswer: "Orange"
+}];
